8dfceba309ebbab59b66f93fb34a2e0590c8c40c
Alessandro FERRO (001076795) <Alessandro.FERRO@Student.TafeSA.edu.au>
2019-04-01 16:19:34 +1030

    Start development feat_ContactDetails

---
+++ assignment/src/InstaRichie/Models/ContactDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite.Net.Attributes;
+
+namespace StartFinance.Models
+{
+    class ContactDetails
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ContactID { get; set; }
+
+        [NotNull]
+        public string ContactFirstName { get; set; }
+
+        [NotNull]
+        public double ContactLastName { get; set; }
+
+        [NotNull]
+        public double ContactCompanyName { get; set; }
+
+        public double ContactMobilePhone { get; set; }
+    }
+}
---
+++ assignment/src/InstaRichie/Models/IContactDetailsRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    interface IContactDetailsRepository
+    {
+        void AddContact();
+        void UpdateContact();
+        void DeleteContact();
+    }
+}
